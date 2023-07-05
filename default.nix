bda3b3d4c058e10ea44c8f2c7972e46e7104bba7
placek <placzynski.pawel@gmail.com>
2023-07-05 12:55:21 +0200

    Introduce stoicheia.nix to the World
               <me> Hello World! This is stoicheia.nix
            <World> Hello stoicheia.nix!
    <stoicheia.nix> Hello World!

---
+++ default.nix
@@ -0,0 +1,59 @@
+{ pkgs ? import <nixpkgs> { }
+, lib ? import <nixpkgs/lib>
+, modules ? [ ]
+, config
+, ...
+}:
+let
+  mainModule = { config, lib, ... }: {
+    imports = modules;
+
+    options = with lib; {
+      name = mkOption {
+        type = types.str;
+        example = "my-project";
+        description = ''
+          The code name of the project.
+        '';
+      };
+
+      version = mkOption {
+        type = types.separatedString ".";
+        example = "1.2.0.4-rc2";
+        description = ''
+          The version of the project.
+        '';
+      };
+
+      helpers = mkOption {
+        type = types.attrsOf (types.functionTo types.anything);
+        default = { };
+        description = ''
+          The helper functions.
+        '';
+      };
+
+      packages = mkOption {
+        type = types.attrsOf types.package;
+        default = { };
+        description = ''
+          The dependencies of the project.
+          This is the place to define packages widely used throughout whole project configuration.
+        '';
+      };
+
+      targets = mkOption {
+        type = types.attrsOf types.package;
+        default = { };
+        description = ''
+          The output build targetss of the project.
+          This is the place to define derivations that can be used by CLI.
+        '';
+      };
+    };
+  };
+in
+(lib.evalModules {
+  modules = [ mainModule config ];
+  specialArgs = { inherit pkgs lib; };
+}).config
